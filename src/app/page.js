92011d0fcbf08c10e8c00b2fea47001ee6b0a99b
Ismingiz <email@misol.com>
2025-02-11 12:07:56 +0500

    .

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,12 @@
 import Main from "@/components/Main";
 import Navbar from "@/components/Navbar";
 import { useRouter } from "next/navigation";
-// import { useRouter } from "next/router";
-// import { Main } from "next/document";
+
 import React from "react";
 
 function Home() {
   const route = useRouter();
-  // if (!localStorage.getItem("accessToken")) {
-  //   route.push("/login");
-  // }
+
   return (
     <div>
       {/* <Navbar /> */}
